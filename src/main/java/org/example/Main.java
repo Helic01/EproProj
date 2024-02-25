4cfdfa6a907cdbd1a7d4e08e3ab0748b0060fa9b
ilovedemocracy <christoph.ebert@kabelmail.de>
2024-02-25 20:50:14 +0100

    Spring-Boot impl

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,20 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
 public class Main {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        Hans x = new Hans();
+        SpringApplication.run(Main.class, args);
     }
 }
