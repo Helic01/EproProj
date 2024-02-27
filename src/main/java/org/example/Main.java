ab31b35a51b66dc295bc58b98e93a2e82acbe4fe
Helic01 <121826798+Helic01@users.noreply.github.com>
2024-02-27 21:13:26 +0100

    nichts

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @ComponentScan(basePackages = {"com.example.config", "com.example.controller", "com.example.model", "com.example.repository", "com.example.service"})
-public class Main {
+public class Main {//v
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
