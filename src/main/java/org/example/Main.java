5455dfdba6dc8af3608c833fe6f8955be6460f21
Helic01 <121826798+Helic01@users.noreply.github.com>
2024-02-28 01:06:20 +0100

    Cleanup +to String Methods +comments handled some edge cases

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @ComponentScan(basePackages = {"com.example.config", "com.example.controller", "com.example.model", "com.example.repository", "com.example.service"})
-public class Main {//v
+public class Main {//v1.0
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
