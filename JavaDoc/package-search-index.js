f3d801b846de3e639bbfd0a77ea45bb06ff178b9
Helic01 <121826798+Helic01@users.noreply.github.com>
2024-02-28 20:55:43 +0100

    Javadoc and PPP

---
+++ JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.example"},{"l":"org.example.controller"},{"l":"org.example.justchecking"},{"l":"org.example.model"},{"l":"org.example.repository"},{"l":"org.example.service"}];updateSearchResults();
